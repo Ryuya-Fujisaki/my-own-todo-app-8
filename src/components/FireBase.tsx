a81a435aa15d4b55ae71b914125c127da29be497
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2023-08-14 17:55:15 +0800

    added .env file

--- src/components/FireBase.tsx
+++ src/components/FireBase.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoqPSpMkOcaepiFVhum3iIwN_sqh2yAXA",
-    authDomain: "fir-todo-a89e6.firebaseapp.com",
-    projectId: "fir-todo-a89e6",
-    storageBucket: "fir-todo-a89e6.appspot.com",
-    messagingSenderId: "328076594397",
-    appId: "1:328076594397:web:0ee801d0cf079ca7735f34"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
